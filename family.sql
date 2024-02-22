77aee8a4e674479f9741f8fe5697e78a44f0d401
marborjamoll <148994650+marborjamoll@users.noreply.github.com>
2024-02-22 17:12:04 +0100

    Update family.sql
    family members updated

--- family.sql
+++ family.sql
@@ -9,11 +9,11 @@ CREATE TABLE Persons (
 );
 
 INSERT INTO Persons (first_name, last_name, birthdate, gender, mother_id, father_id) VALUES
-('John', 'Doe', '1980-01-01', 'M', null, null),
-('Jane', 'Doe', '1982-02-02', 'F', null, null),
-('Jim', 'Doe', '2010-03-03', 'M', 2, 1),
-('Jill', 'Doe', '2012-04-04', 'F', 2, 1),
 ('Alice', 'Smith', '1955-05-05', 'F', null, null), -- Assuming Jane's mother
 ('Robert', 'Smith', '1950-06-06', 'M', null, null), -- Assuming Jane's father
+('John', 'Doe', '1980-01-01', 'M', null, null),
+('Jane', 'Doe', '1982-02-02', 'F', null, null),
+('Jim', 'Doe', '2010-03-03', 'M', 4, 3),
+('Jill', 'Doe', '2012-04-04', 'F', 4, 3),
 ('Michael', 'Johnson', '1978-07-07', 'M', null, null), -- Assuming unrelated person
 ('Emily', 'Johnson', '2008-08-08', 'F', null, 7); -- Child of Michael
